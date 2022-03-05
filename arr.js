65a0eeecd12f0baab665910e43592459ffd5ef51
neelLogan19 <neelkanth1999@gmail.com>
2022-03-05 23:08:55 +0530

    array in javascript

---
+++ arr.js
@@ -0,0 +1,18 @@
+//arrays in javascript
+
+let arr=[];
+console.log(arr);
+//indexing is 0 based
+
+let arrnew= [1,2,3,4.56,"i am array"];
+console.log(arrnew);
+
+// printing particular element in array 
+console.log("the element is" + arrnew[2] );
+
+//changing element in array
+ 
+
+arrnew[2]= "nothing";
+console.log(arrnew);
+
