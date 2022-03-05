318b38d816788cda2adbec06a24ff20d6d1b0e4b
neelLogan19 <neelkanth1999@gmail.com>
2022-03-05 23:54:20 +0530

    String methods

--- arr.js
+++ arr.js
@@ -23,3 +23,37 @@ console.log("array before pushing"+ arrnew);
 arrnew.push("i am pushed :)");
 console.log("array after pushing" + arrnew);
 
+
+//practice code
+// let brr=[1,4,3,,6,78,4,5,7,8];
+// let flag = 0;
+// for(let i =0;i<brr.length;i++){
+//     if(brr[i]%2==0){
+//         flag=1;
+//     }
+// }
+// if(flag==1){
+//     console.log(brr[i]+"Even");
+// }else{
+//     console.log("odd");
+// }
+
+//2-Pop method-Removes the last element from the list
+console.log("array before"+arrnew);
+arrnew.pop();
+console.log("array after" + arrnew);
+
+
+
+//3- shift- Removes first element from the list
+console.log("array before" + arrnew);
+arrnew.shift();
+console.log("array after" + arrnew);
+
+
+
+//4- Unshift- Adds element to the start
+console.log("array before" + arrnew);
+arrnew.unshift("i am the new element");
+console.log("array after" + arrnew);
+
