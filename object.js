05b7dc27686334edef91d3c27a09fa21b82460db
neelLogan19 <neelkanth1999@gmail.com>
2022-03-08 21:53:10 +0530

    learning objects

---
+++ object.js
@@ -0,0 +1,11 @@
+//objects- similar to dictionary in pyhton 
+
+let obneel={
+    name:'Neelkanth vashist',
+    age:21,
+    PhoneNo:8920432940,
+    address:'Palam vihar'
+
+}
+
+console.log(obneel);
